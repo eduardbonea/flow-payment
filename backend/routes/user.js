ac55af100965aa22ffa6e6efdb2bf9f7928cb208
eduard-linux <eduardbonea@gmail.com>
2025-11-29 15:17:02 +0200

    finished user controllers and routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,11 @@ const express = require('express');
 const userRouter = express.Router();
 const {userController} = require('../controllers');
 
-userRouter.post('/createUser', userController.createUser);
+userRouter.post('/create', userController.create);
+userRouter.get('/getid/:id', userController.getById);
+userRouter.patch('/patchusername/:id', userController.updateUsername);
+userRouter.patch('/patchpassword/:id', userController.updatePassword);
+userRouter.patch('/patchemail/:id', userController.updateEmail);
+userRouter.delete('/delete/:id', userController.delete);
 
 module.exports = userRouter;
